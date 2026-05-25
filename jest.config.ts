82ea63219c4e7140c4d087b92ee203e3e06a50bb
Ariless <dariya_bel@tut.by>
2026-05-25 09:17:09 +0100

    chore: remove premium AI files from public repo

--- jest.config.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  testEnvironment: 'node',
-  testMatch: ['**/pact/**/*.test.ts', '**/ai-properties/**/*.test.ts'],
-  transform: {
-    '^.+\\.ts$': ['ts-jest', { tsconfig: './jest.tsconfig.json' }],
-  },
-}
